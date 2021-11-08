949456dcc77d23e59f017a5caa366e4b591a9b5a
Robert Tan <robert.tan@mail.utoronto.ca>
2021-11-07 19:20:23 -0500

    idk

--- algorithm/env.py
+++ algorithm/env.py
@@ -5,18 +5,14 @@ from games.gardner.GardnerMiniChessGame import GardnerMiniChessGame
 
 import numpy as np
 import gym
-from gym.spaces import Discrete, Box, Dict
 
 from games.gardner.GardnerMiniChessLogic import Board
 
 class MinichessEnv(gym.Env):
     def __init__(self, config) -> None:
         self.reset()
-        self.action_space = Discrete(self.game.getActionSize())
-        self.observation_space = Dict({
-            "obs": Box(-60000, 60000, shape=(5,5)),
-            "action_mask": Box(low=0, high=1, shape=(self.action_space.n, )),
-        })
+        self.action_space = gym.spaces.Discrete(self.game.getActionSize())
+        self.observation_space = gym.spaces.Box(-60000, 60000, shape=(5,5))
 
     def reset(self):
         self.game = GardnerMiniChessGame()
@@ -43,4 +39,4 @@ class MinichessEnv(gym.Env):
         return set(legal_moves)
         
     def _obs(self):
-        return { "obs": self.board, "action_mask": np.ones((self.action_space.n,)) }
+        return self.board
