df4fac4d4a6276618ef2528176a7e93dd0eb6593
Michael Sun <michaelsun18@yahoo.com>
2021-12-04 15:26:46 -0800

    new branch extending vm gpu for single agent to validate critic

--- train.py
+++ train.py
@@ -54,6 +54,7 @@ if __name__ == "__main__":
     config["train_batch_size"]=1000
     config["sgd_minibatch_size"]=100
     config["entropy_coeff"]=0.00
+    config["gamma"]=0.5
     config["lr"] = 1e-5
 
     stop = {
