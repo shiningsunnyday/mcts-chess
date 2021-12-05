6549782f886709b20509326d017ba0510218e15d
Michael Sun <michaelsun18@yahoo.com>
2021-12-04 18:50:48 -0800

    gamma 0.1 with reward scaled back to 0 to 60

--- mini_train.py
+++ mini_train.py
@@ -350,7 +350,7 @@ class Games(Dataset):
 
 
     def __getitem__(self, idx):
-        return self.data[idx], self.pi[idx], (self.ys[idx]-0.5)*1200 # [0,1] to reward range
+        return self.data[idx], self.pi[idx], (self.ys[idx]-0.5)*120 # [0,1] to reward range [0, 60]
 
     def __len__(self):
         return len(self.ys) 
